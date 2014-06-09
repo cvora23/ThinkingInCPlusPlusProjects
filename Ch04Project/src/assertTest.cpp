e45aa8f94d3c00d2405d55bfd3f4f3bd5350b3b8
cvora23 <cvora22@gmail.com>
2014-06-09 12:55:50 -0700

    Chapter 4 project files

---
+++ Ch04Project/src/assertTest.cpp
@@ -0,0 +1,23 @@
+/*
+ * assertTest.cpp
+ *
+ *  Created on: Jun 9, 2014
+ *      Author: cvora
+ */
+
+#define NDEBUG 1
+
+
+#include <iostream>
+#include <cassert>
+
+
+using namespace std;
+
+int main()
+{
+	assert(false);
+}
+
+
+
