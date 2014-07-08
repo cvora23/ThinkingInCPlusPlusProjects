67c33b78c4a3ecd064ba5da6a4705928343fd0a4
cvora23 <cvora22@gmail.com>
2014-07-08 15:34:36 -0700

    Chapter 16 Project

---
+++ Ch16Project/src/StackTemplateTest.cpp
@@ -0,0 +1,43 @@
+//: C16:StackTemplateTest.cpp
+// From Thinking in C++, 2nd Edition
+// Available at http://www.BruceEckel.com
+// (c) Bruce Eckel 2000
+// Copyright notice in Copyright.txt
+// Test simple stack template
+//{L} fibonacci
+#include "fibonacci.h"
+#include "StackTemplate.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+class A{
+
+};
+
+int main() {
+  StackTemplate<int> is;
+  for(int i = 0; i < 20; i++)
+    is.push(fibonacci(i));
+  for(int k = 0; k < 20; k++)
+    cout << is.pop() << endl;
+  ifstream in("StackTemplateTest.cpp");
+  assure(in, "StackTemplateTest.cpp");
+  string line;
+  StackTemplate<string> strings;
+  while(getline(in, line))
+    strings.push(line);
+  while(strings.size() > 0)
+    cout << strings.pop() << endl;
+
+  A a[] = { A(), A()};
+
+  StackTemplate<A>AObjects;
+  for(unsigned int i=0;i<sizeof(a)/sizeof(a[0]);i++)
+	  AObjects.push(a[i]);
+
+//  for(unsigned int i=0;i<sizeof(a)/sizeof(a[0]);i++)
+//	  cout<<AObjects.pop()<<endl;
+
+} ///:~
